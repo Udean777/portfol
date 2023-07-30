6d1214955983c3a43648ea7963c62d45ec0bcf01
Udean777 <uaart123@gmail.com>
2023-07-30 14:35:51 +0700

    ngatur responsive

--- app/components/NavPills.js
+++ app/components/NavPills.js
@@ -35,7 +35,7 @@ const NavPills = ({ tabs }) => {
   };
 
   return (
-    <div className="flex justify-center md:py-20 lg:py-20 font-sans">
+    <div className="flex justify-center md:py-5 lg:py-5 font-sans">
       <div className="w-95 md:w-98 lg:w-100">
         <div className="py-10">
           <h1 className="font-bold border-l-4 border-blue-500 text-3xl sm:text-4xl md:text-3xl">
