33d361584861b7543115d3e7c4b852da3042adc7
Udean777 <uaart123@gmail.com>
2023-08-02 16:35:55 +0700

    edit layout

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,20 @@
-import Particle from './components/Particles';
-import './globals.css'
-import { Analytics } from '@vercel/analytics/react';
+import Particle from "./components/Particles";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: `Sajudin Ma'ruf`,
-  description: 'This web is made with 💙 by Udean',
-}
+  description: "This web is made by Udean",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className='bg-gradient-to-b from-slate-300 to-slate-200 dark:bg-gradient-to-b dark:from-slate-600 dark:to-slate-500'>
-        <Particle/>
+      <body className="bg-gradient-to-b from-slate-300 to-slate-200 dark:bg-gradient-to-b dark:from-slate-600 dark:to-slate-500">
+        <Particle />
         {children}
-        <Analytics/>
-        </body>
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
