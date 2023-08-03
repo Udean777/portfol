ec2e2210f2e63aaaa67f6199a2640a88d21e5627
Udean777 <uaart123@gmail.com>
2023-08-03 11:46:49 +0700

    nambah & edit projects sec

--- app/projects/page.js
+++ app/projects/page.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Link from "next/link";
 import { TbBrandMongodb, TbBrandNextjs, TbBrandTailwind } from "react-icons/tb";
-import { FaBootstrap, FaNodeJs, FaReact } from "react-icons/fa";
+import { FaBootstrap, FaGithub, FaNodeJs, FaReact } from "react-icons/fa";
 import { SiExpress } from "react-icons/si";
 import "animate.css";
 
@@ -10,7 +10,7 @@ export default function Projects() {
   return (
     <div className="min-h-screen">
       <Navbar />
-      <div className="m-auto py-10 sm:py-20 md:py-28 max-w-6xl px-4 sm:px-6 md:px-8 animate__animated animate__fadeInDown text-stone-800 dark:text-stone-200 overflow-hidden">
+      <div className="m-auto py-5 sm:py-10 max-w-6xl px-4 sm:px-6 md:px-8 animate__animated animate__fadeInDown text-stone-800 dark:text-stone-200 overflow-hidden">
         <h1 className="font-bold border-l-4 border-blue-500 text-3xl sm:text-4xl md:text-3xl mb-6 sm:mb-8">
           PROJECTS
         </h1>
@@ -101,12 +101,7 @@ export default function Projects() {
 
           <Link href="https://github.com/Udean777/todo-deploy" target="__blank">
             <div className="block w-full group hover:scale-105 transition ease-in rounded-xl overflow-hidden shadow-md backdrop-blur-sm bg-zinc-200/30">
-              <img
-                loading="lazy"
-                className="w-full rounded-sm "
-                src="/project4.png"
-                alt="Card Image"
-              />
+              <FaGithub className="flex justify-center m-auto p-2" size={106} />
               <div className="px-6 py-4">
                 <div className="font-bold text-md mb-2">To Do List</div>
                 <div className="flex gap-2">
@@ -125,6 +120,30 @@ export default function Projects() {
                   />
                   <FaNodeJs
                     className="text-green-500 bg-slate-500 p-1 rounded-full"
+                    size={30}
+                  />
+                </div>
+              </div>
+            </div>
+          </Link>
+
+          <Link href="https://github.com/Udean777/next-crud" target="__blank">
+            <div className="block w-full group hover:scale-105 transition ease-in rounded-xl overflow-hidden shadow-md backdrop-blur-sm bg-zinc-200/30">
+              <FaGithub className="flex justify-center m-auto p-2" size={106} />
+              <div className="px-6 py-4">
+                <div className="font-bold text-md mb-2">CRUD w/Next JS</div>
+                <div className="flex gap-2">
+                  {" "}
+                  <TbBrandMongodb
+                    className="bg-green-600 p-1 rounded-full text-slate-50"
+                    size={30}
+                  />
+                  <TbBrandNextjs
+                    className="bg-slate-800 text-slate-50 dark:bg-slate-50 dark:text-slate-800 p-1 rounded-full"
+                    size={30}
+                  />
+                  <FaReact
+                    className="text-blue-600 dark:bg-slate-50 p-1 rounded-full"
                     size={30}
                   />
                 </div>
